537ab9dc7c15d86e30faac5247419bd4c6e549f7
robertcano <rcanoestevez@proton.me>
2025-01-23 14:25:03 +0100

    update

--- ejercicios_POO/primera_tanda/rectangle.py
+++ ejercicios_POO/primera_tanda/rectangle.py
@@ -6,43 +6,43 @@ from point import Point
 class Rectangle:
   
     def __init__(self, p1, p2):
-        self._p1 = p1
-        self._p2 = p2
+        self.__p1 = p1
+        self.__p2 = p2
         self.update_dimensions()
     
     @property
     
     def area(self):
-        return self._width * self._height
+        return self.__width * self.__height
 
     @property
     
     def perimeter(self):
-        return 2 * (self._width + self._height)
+        return 2 * (self.__width + self.__height)
     
     @property
 
     def p1(self):
-        return self._p1
+        return self.__p1
     
     @p1.setter
     
     def p1(self, value):
-        self._p1 = value
+        self.__p1 = value
         self.update_dimensions()
     
     @property
 
     def p2(self):
-        return self._p2
+        return self.__p2
     
     @p2.setter
 
     def p2(self, value):
-        self._p2 = value
+        self.__p2 = value
         self.update_dimensions()
 
     def update_dimensions(self):
 
-        self._width = abs(self._p2.x - self._p1.x)
-        self._height = abs(self._p2.y - self._p1.y)
+        self.__width = abs(self.__p2.x - self.__p1.x)
+        self.__height = abs(self.__p2.y - self.__p1.y)
